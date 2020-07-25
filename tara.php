ac4e2a9df53a9dd5ec8e8de6a71e0026d764c9a5
stonerough <stonerough@users.noreply.github.com>
2020-07-25 18:35:25 +1200

    Update tara.php

--- tara.php
+++ tara.php
@@ -1,11 +1,6 @@
 <!DOCTYPE html>
-window.location.replace("https://newgrove.co.nz");
 <html>
    <head>
-      <title>HTML Meta Tag</title>
       <meta http-equiv = "refresh" content = "0; url = https://newgrove.co.nz" />
    </head>
-   <body>
-      <p>Hello HTML5!</p>
-   </body>
 </html>
